f927f8e149216b1b1b2b70b8a6d87e090c4f1bfc
Rafael Motta <34509225+RafaMotta@users.noreply.github.com>
2022-03-31 17:14:10 -0300

    wip

--- nestjs-api/src/app.controller.ts
+++ nestjs-api/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('prefixo')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('teste')
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('teste1')
+  getHello1(): string {
+    return this.appService.getHello();
+  }
+  @Get('teste2')
+  getHello2(): string {
+    return 'Hello World diferente';
   }
 }
